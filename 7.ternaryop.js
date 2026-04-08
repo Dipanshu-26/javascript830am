f80263e21cbd196904ed2c02d5cf5c1faa438c39
Dipanshu-26 <dnchawde@gmail.com>
2026-04-08 08:39:56 +0530

    first commit

---
+++ 7.ternaryop.js
@@ -0,0 +1,28 @@
+//ternary operator 
+//if -else
+//short hand 
+
+//greater number
+
+let a=100
+let b=200
+if(a>b){
+    console.log("a is bigger")
+}
+else {
+    console.log("b is bigger")
+}
+console.log("--------------------------")
+//ternary operator 
+
+//condition ? true -execute statement 1 : false  -executu statement 2
+//greater number
+
+a>b ? console.log("a is bigger") : console.log("b is bigger")
+
+
+//can drive ==> age 
+let age = 6
+
+//condition ? true -execute statement 1 : false  -executu statement 2
+age >=18 ? console.log("can drive") : console.log("can not drive")
