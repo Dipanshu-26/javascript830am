5c047e51d0c6b40b6c5ec8693fd6b5439ba54b12
Dipanshu-26 <dnchawde@gmail.com>
2026-04-13 09:01:18 +0530

    13-Apr-26

--- 8.switchcase.js
+++ 8.switchcase.js
@@ -3,60 +3,175 @@
 
 //one input -- multiple output 
 
-let city = "pune"
+// let city = "pune"
 
-switch (city) {
-    case "pune":
-        console.log("MH")
-    case "delhi":
-        console.log("delhi")
-    case "banglore":
+// switch (city) {
+//     case "pune":
+//         console.log("MH")
+//     case "delhi":
+//         console.log("delhi")
+//     case "banglore":
+//         console.log("KA")
+//     case "jaipur":
+//         console.log("RA")
+// }
+
+// //--------------------------------------------------------
+// console.log("----------------------")
+// city = "pune"
+
+// switch (city) {
+//     case "pune":
+//         console.log("MH")
+//         break
+//     case "delhi":
+//         console.log("delhi")
+//         break
+//     case "banglore":
+//         console.log("KA")
+//         break
+//     case "jaipur":
+//         console.log("RA")
+//         break
+//     default:
+//         console.log("incorrect name...")
+// }
+
+// console.log("----------------------")
+// city = "Pune"
+
+// switch (city) {
+//     case "pune":
+//     case "Pune":
+//         console.log("MH")
+//         break
+//     case "delhi":
+//     case "Delhi":    
+//         console.log("delhi")
+//         break
+//     case "banglore":
+//     case "Banglore":    
+//         console.log("KA")
+//         break
+//     case "jaipur":
+//     case "Jaipur":
+//         console.log("RA")
+//         break
+//     default:
+//         console.log("incorrect name...")
+// }
+
+//-------------------------------------------------------------------------------------
+//revision
+//conditional operators 
+//if - else , ternary op , swich case 
+
+//one input -- multiple output 
+
+let city = "mumbai"
+
+switch(city){
+    case "banglore" :
         console.log("KA")
     case "jaipur":
-        console.log("RA")
+        console.log("RJ")
+    case "Bhopal":
+        console.log("MP")
+    case "pune":
+        console.log("MH")            
 }
 
-//--------------------------------------------------------
-console.log("----------------------")
+//break and default
+
+city = "banglore"
+
+switch(city){
+    case "banglore" :
+        console.log("KA")
+        break
+    case "jaipur":
+        console.log("RJ")
+        break
+    case "Bhopal":
+        console.log("MP")
+        break
+    case "pune":
+        console.log("MH")
+        break  
+    default :
+        console.log("please enter valid city name...")    
+
+}
+
+//------------------------------------------
+city = "bhopal"
+switch(city){
+    case "banglore":
+    case "Banglore":
+        console.log("KA") 
+        break 
+      case "jaipur":
+      case "Jaipur":
+        console.log("RJ")
+        break
+    case "bhopal":
+    case "Bhopal":    
+        console.log("MP")
+        break
+    case "pune":
+    case "Pune":    
+        console.log("MH")
+        break  
+    default :
+        console.log("please enter valid city name...")        
+}
+
+//---------------------------------------------------------------
+console.log("----------------------------------------")
+
 city = "pune"
-
-switch (city) {
-    case "pune":
+switch(city){                                    //switch(pune)
+    case (city == "pune" || city == "Pune"):    //case : true
         console.log("MH")
         break
-    case "delhi":
-        console.log("delhi")
-        break
-    case "banglore":
-        console.log("KA")
-        break
-    case "jaipur":
-        console.log("RA")
-        break
-    default:
-        console.log("incorrect name...")
+
+    case (city == "Bhopal" || city == "bhopal"):   //case :false
+        console.log("MH")
+        break 
+        
+     case (city == "banglore" || city ==  "Banglore"):
+        console.log("KA") 
+        break 
+
+      case (city == "jaipur" || city == "Jaipur"):
+        console.log("RJ")
+        break  
+
+    default :
+        console.log("please enter valid city name...")    
 }
 
-console.log("----------------------")
-city = "Pune"
+console.log("----------------------------------------")
 
-switch (city) {
-    case "pune":
-    case "Pune":
+city = "jaipur"
+isFlag = true
+
+switch(isFlag){                                   //switch(true)
+    case (city == "pune" || city == "Pune"):    //case : false
+        break
+
+    case (city == "Bhopal" || city == "bhopal"):   //case :false
         console.log("MH")
+        break  
+        
+     case (city == "banglore" || city ==  "Banglore"):   //case : false
+        console.log("KA") 
+        break 
+
+      case (city == "jaipur" || city == "Jaipur"):      //case : true
+        console.log("RJ")
         break
-    case "delhi":
-    case "Delhi":    
-        console.log("delhi")
-        break
-    case "banglore":
-    case "Banglore":    
-        console.log("KA")
-        break
-    case "jaipur":
-    case "Jaipur":
-        console.log("RA")
-        break
-    default:
-        console.log("incorrect name...")
+
+    default :
+        console.log("please enter valid city name...")    
 }
