da0d2d1d56c734c211b04d5901e6e60a459a5765
Dipanshu-26 <dnchawde@gmail.com>
2026-05-08 22:20:02 +0530

    8-May

---
+++ map.js
@@ -0,0 +1,149 @@
+//js datatypes 
+//number
+//string 
+//boolean
+//arrays
+//objects
+//map 
+//set
+
+let a=10.5
+console.log(typeof(a))
+
+a="dipanshu"
+console.log(typeof(a))
+
+a=true
+console.log(typeof(a))
+
+a=["dipanshu",11,true]
+console.log(typeof(a))
+
+a={fn : "dipanshu", ln : "chawde"}
+console.log(typeof(a))
+
+//map 
+//key , value 
+
+let map1 = new Map([
+    ["fn" , "dipanshu"],
+    ["ln" , "chawde"],
+    [1, "javascript"],
+    [true , "hasVechicle"]
+])
+console.log(map1)
+
+//-----------------------------------------------------
+let map2 = new Map()
+
+map2.set("fn" ,"dipanshu")
+map2.set("ln" ,"chawde")
+map2.set(true,"hasVechicle")
+map2.set([11,22,33],"marks")
+map2.set({fn : "dip"},"info")
+console.log(map2)
+//-----------------------------------------------------
+//retrive
+
+console.log(map2.get("fn"))
+console.log(map2.get("ln"))
+console.log(map2.get(true))
+console.log(map2.get([11,22,33]))     //undefined
+console.log(map2.get({fn : "dip"}))   //undefined
+
+//--------------------------------------------------------
+console.log(2==2)
+console.log("dip" == "dip")
+console.log([1,2,3] == [1,2,3])     //false
+console.log({fn : "dip"} == {fn : "dip"})   //reference 
+//----------------------------------------------------------
+
+let map3 = new Map([
+    [1,"admin"],
+    [2,"manager"],
+    [3,"customer"]
+])
+
+//retrive
+console.log(map3)
+console.log(map3.get(1))
+let q1 = map3.get(2)
+console.log(q1)
+
+//add 
+map3.set(4,"trainee")
+console.log(map3)
+
+map3.set(5,"project manager")
+console.log(map3)
+
+//update
+map3.set(5,"leader")
+console.log(map3)
+
+//delete
+let q2 =map3.delete(4)
+console.log(map3)
+console.log(q2)
+
+let q3 =map3.delete(6)
+console.log(map3)
+console.log(q3)
+
+map3.set(6,"PM")
+console.log(map3)
+
+let arr= [1,2,3,4]
+console.log(arr)
+
+//if any key exists?
+let q4 = map3.has(5)
+console.log(q4)
+
+q4 = map3.has(4)
+console.log(q4)
+
+//looping
+for(let k of map3.keys()){
+    console.log(k)
+}
+
+for(let k of map2.keys()){
+    console.log(k)
+}
+
+for(let v of map3.values()){
+    console.log(v)
+}
+
+for(let e of map3.entries()){
+    console.log(e)
+}
+
+
+for(let [k,v] of map2){
+    console.log(k , v )
+}
+
+map3.forEach(function(v,k){
+    console.log(v,k)
+})
+
+//------------------------------------------------
+let names = ["dipanshu","nitin","neel"]
+let response = [
+    {id :1,name : "aaa"},
+    {id :2,name : "bbb"},
+    {id :3,name : "ccc"},
+    {id :4,name : "ddd"}
+]
+
+// 1 , "aaa" , 2 , "bbb" 3, "ccc"
+
+let map4 = new Map()
+
+response.forEach(function(el){
+    //     k     v
+    map4.set(el.id, el.name)
+})
+console.log(map4)
